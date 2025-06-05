f06cbda444c303da3620ab33fb884094d471c6b8
Enay Zuin <enayzuin@gmail.com>
2025-06-05 16:25:03 -0300

    feat: change apikeys usage

--- config.py
+++ config.py
@@ -2,17 +2,23 @@
 Arquivo de configuração para as APIs utilizadas no projeto.
 """
 
+import os
+from dotenv import load_dotenv
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
 # Configuração da API OpenAI
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Configuração da NewsAPI
-NEWSAPI_API_KEY = "87efc86660e541ab8b3d732a13dd3f13"
+NEWSAPI_API_KEY = os.getenv("NEWSAPI_API_KEY")
 
 # Configuração do WordPress
-WORDPRESS_URL = "https://emaltaagora.com/xmlrpc.php"
-WORDPRESS_USERNAME = "bottrends"
-WORDPRESS_PASSWORD = "rjMG 9DLN ezi6 s4wv 1h7X rlmQ"
+WORDPRESS_URL = os.getenv("WORDPRESS_URL")
+WORDPRESS_USERNAME = os.getenv("WORDPRESS_USERNAME")
+WORDPRESS_PASSWORD = os.getenv("WORDPRESS_PASSWORD")
 
 # Configurações gerais
-MAX_TRENDS = 5  # Número máximo de tendências a serem processadas
-MAX_NEWS_PER_TREND = 3  # Número máximo de notícias por tendência
+MAX_TRENDS = int(os.getenv("MAX_TRENDS", 5))  # Número máximo de tendências a serem processadas
+MAX_NEWS_PER_TREND = int(os.getenv("MAX_NEWS_PER_TREND", 3))  # Número máximo de notícias por tendência
